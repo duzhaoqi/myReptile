2fd18c86a94c06af16324fecb0f57dbd5253e8a3
duzhaoqi <linuxbugs@163.com>
2019-11-15 18:20:10 +0800

    update

---
+++ 01Requset/01req.py
@@ -0,0 +1,15 @@
+import requests
+
+headers={"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36"}
+
+try:
+    response = requests.get("https://www.baidu.com/",headers=headers,timeout=1)
+    print(response.status_code)
+    #content 返回字节流数据
+    # print(response.content.decode("UTF-8"))
+    # print(response.url)
+    # print(response.encoding)
+    # print(response.headers)
+    # print(response.request.headers)
+except Exception as e:
+    print(e)
