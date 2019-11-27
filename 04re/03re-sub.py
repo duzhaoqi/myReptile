674a3d0c28922c108c9516b290a764ebf699e5c7
duzhaoqi <linuxbugs@163.com>
2019-11-27 21:26:22 +0800

    update

--- 04re/03re-sub.py
+++ 04re/03re-sub.py
@@ -3,7 +3,7 @@ import re
 
 regex = re.compile(r'(\w+) (\w+)')
 s1 = 'one12twothree34four'
-s2 = 'hello hello2 233456.765'
+s2 = 'hello hello2 233 456.765'
 
 print(regex.sub(r'qqq qqq',s2))
 print(regex.sub(r'\2 \1',s2))
@@ -11,4 +11,4 @@ print(regex.sub(r'\2 \1',s2))
 def func(m):
     return 'HI'+ ' '+m.group(1)
 
-print(regex.sub(func,s2))
+print(regex.sub(func,s2,1)) #里面的1代表只替换一次
